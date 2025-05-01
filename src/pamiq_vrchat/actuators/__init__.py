f615e0696170144a8d8b345f5be2b7b160c2296f
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-05-01 02:24:56 +0000

    chore: expose actuators to `actuators` name space

---
+++ src/pamiq_vrchat/actuators/__init__.py
@@ -0,0 +1,24 @@
+from .mouse import MouseAction, MouseActuator, MouseButton, SmoothMouseActuator
+from .osc import (
+    Axes,
+    AxesAction,
+    Buttons,
+    ButtonsAction,
+    OscAction,
+    OscActuator,
+    SmoothOscActuator,
+)
+
+__all__ = [
+    "MouseAction",
+    "MouseButton",
+    "MouseActuator",
+    "SmoothMouseActuator",
+    "OscAction",
+    "OscActuator",
+    "SmoothOscActuator",
+    "Axes",
+    "Buttons",
+    "AxesAction",
+    "ButtonsAction",
+]
