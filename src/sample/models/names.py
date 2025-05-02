b8392d486782a50bc3e1dc4afbc74ffa24a2e23b
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-05-02 01:28:35 +0000

    feat(models): register model names

---
+++ src/sample/models/names.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+
+class ModelName(StrEnum):
+    """Enumerates all model names in experiments."""
+
+    JEPA_CONTEXT_ENCODER = "jepa_context_encoder"
+    JEPA_TARGET_ENCODER = "jepa_target_encoder"
+    JEPA_PREDICTOR = "jepa_predictor"
