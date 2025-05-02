ce5b64bc01cee07de423947dc972dfc1e53bf496
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-05-02 01:07:58 +0000

    feat(env): implement VRChatEnvironment

--- src/pamiq_vrchat/__init__.py
+++ src/pamiq_vrchat/__init__.py
@@ -1,7 +1,22 @@
 from importlib import metadata
 
 from . import actuators, sensors
+from .env import (
+    ActionType,
+    ObservationType,
+    VRChatAction,
+    VRChatEnvironment,
+    VRChatObservation,
+)
 
 __version__ = metadata.version(__name__.replace("_", "-"))
 
-__all__ = ["actuators", "sensors"]
+__all__ = [
+    "actuators",
+    "sensors",
+    "ActionType",
+    "ObservationType",
+    "VRChatAction",
+    "VRChatObservation",
+    "VRChatEnvironment",
+]
