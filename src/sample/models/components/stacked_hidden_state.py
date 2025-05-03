f2e9e707111194917d153e39acecffb594cc763c
myxyy <m.myxomatosis@gmail.com>
2025-05-03 12:09:06 +0000

    improment sioconvps

---
+++ src/sample/models/components/stacked_hidden_state.py
@@ -0,0 +1,76 @@
+from typing import override
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+class StackedHiddenState(nn.Module):
+    def __init__(self, module_list: nn.ModuleList):
+        super().__init__()
+        self.module_list = module_list
+
+    # (*batch, len, dim), (*batch, depth, dim) -> (*batch, len, dim), (*batch, depth, len, dim) or
+    # (len, dim), (depth, dim) -> (len, dim), (depth, len, dim) or
+    # (*batch, dim), (*batch, depth, dim) -> (*batch, dim), (*batch, depth, dim) or
+    # (dim), (depth, dim) -> (dim), (depth, dim)
+    @override
+    def forward(self, x: Tensor, hidden_stack: Tensor) -> tuple[Tensor, Tensor]:
+        no_batch = len(hidden_stack.shape) < 3
+        if no_batch:
+            x = x.unsqueeze(0)
+            hidden_stack = hidden_stack.unsqueeze(0)
+
+        no_len = len(x.shape) < 3
+        if no_len:
+            x = x.unsqueeze(1)
+
+        batch_shape = x.shape[:-2]
+        x = x.reshape(-1, *x.shape[len(batch_shape) :])
+        hidden_stack = hidden_stack.reshape(-1, *hidden_stack.shape[len(batch_shape) :])
+
+        hidden_out_list = []
+        for i, module in enumerate(self.module_list):
+            x, hidden_out = module(x, hidden_stack[:, i, :])
+            hidden_out_list.append(hidden_out)
+
+        hidden_out_stack = torch.stack(hidden_out_list).transpose(1, 0)
+
+        x = x.view(*batch_shape, *x.shape[1:])
+        hidden_out_stack = hidden_out_stack.view(
+            *batch_shape, *hidden_out_stack.shape[1:]
+        )
+
+        if no_len:
+            x = x.squeeze(1)
+            hidden_out_stack = hidden_out_stack.squeeze(2)
+
+        if no_batch:
+            x = x.squeeze(0)
+            hidden_out_stack = hidden_out_stack.squeeze(0)
+
+        return x, hidden_out_stack
+
+
+class StackedHiddenSelection(nn.Module):
+    """Selectively maps stacked hidden states to an arbitrary number of
+    stacks."""
+
+    def __init__(self, in_stacks: int, out_stacks: int) -> None:
+        super().__init__()
+        # Initially averages along the stack.
+        self._weight = nn.Parameter(torch.zeros(in_stacks, out_stacks))
+
+    @override
+    def forward(self, hidden_stack: Tensor) -> Tensor:
+        """
+        Args:
+            hidden_stack (Tensor): shape is (*, in_stacks, dim)
+
+        Returns:
+            Tensor: shape is (*, out_stacks, dim)
+        """
+        hidden_stack = hidden_stack.transpose(-1, -2)
+        hidden_stack = hidden_stack @ self._weight.softmax(0)
+        hidden_stack = hidden_stack.transpose(-1, -2)
+        return hidden_stack
---
+++ tests/sample/models/components/test_stacked_hidden_state.py
@@ -0,0 +1,88 @@
+import pytest
+import torch
+
+from src.sample.models.components.sioconvps import SioConvPS
+from src.sample.models.components.stacked_hidden_state import StackedHiddenSelection
+
+BATCH = 4
+DEPTH = 8
+DIM = 16
+DIM_FF_HIDDEN = 32
+LEN = 64
+DROPOUT = 0.1
+
+
+class TestStackedHiddenState:
+    @pytest.fixture
+    def sioconv(self):
+        sioconv = SioConvPS(DEPTH, DIM, DIM_FF_HIDDEN, DROPOUT)
+        return sioconv
+
+    def test_batch_len(self, sioconv):
+        x_shape = (BATCH, LEN, DIM)
+        x = torch.randn(*x_shape)
+        hidden_shape = (BATCH, DEPTH, LEN, DIM)
+        hidden = torch.randn(*hidden_shape)
+
+        x, hidden = sioconv(x, hidden[:, :, -1, :])
+        assert x.shape == x_shape
+        assert hidden.shape == hidden_shape
+
+    def test_no_batch_len(self, sioconv):
+        x_shape = (LEN, DIM)
+        x = torch.randn(*x_shape)
+        hidden_shape = (DEPTH, LEN, DIM)
+        hidden = torch.randn(*hidden_shape)
+
+        x, hidden = sioconv(x, hidden[:, -1, :])
+        assert x.shape == x_shape
+        assert hidden.shape == hidden_shape
+
+    def test_batch_no_len(self, sioconv):
+        x_shape = (BATCH, DIM)
+        x = torch.randn(*x_shape)
+        hidden_shape = (BATCH, DEPTH, DIM)
+        hidden = torch.randn(*hidden_shape)
+
+        x, hidden = sioconv(x, hidden)
+        assert x.shape == x_shape
+        assert hidden.shape == hidden_shape
+
+    def test_no_batch_no_len(self, sioconv):
+        x_shape = (DIM,)
+        x = torch.randn(*x_shape)
+        hidden_shape = (DEPTH, DIM)
+        hidden = torch.randn(*hidden_shape)
+
+        x, hidden = sioconv(x, hidden)
+        assert x.shape == x_shape
+        assert hidden.shape == hidden_shape
+
+    def test_many_batch_shape(self, sioconv):
+        x_shape = (1, 2, 3, BATCH, LEN, DIM)
+        x = torch.randn(*x_shape)
+        hidden_shape = (1, 2, 3, BATCH, DEPTH, LEN, DIM)
+        hidden = torch.randn(*hidden_shape)
+
+        x, hidden = sioconv(x, hidden[:, :, :, :, -1])
+        assert x.shape == x_shape
+        assert hidden.shape == hidden_shape
+
+
+class TestStackedHiddenSelection:
+    @pytest.fixture
+    def layer(self):
+        return StackedHiddenSelection(10, 8)
+
+    @pytest.mark.parametrize(
+        "input_shape,output_shape",
+        [
+            ((10, 16), (8, 16)),
+            ((2, 10, 32), (2, 8, 32)),
+            ((2, 3, 10, 16), (2, 3, 8, 16)),
+        ],
+    )
+    def test_forward(self, layer, input_shape, output_shape):
+        hidden_stack = torch.randn(input_shape)
+        out = layer(hidden_stack)
+        assert out.shape == output_shape
